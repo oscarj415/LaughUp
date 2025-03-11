e4eeed5bdbec5646d2852d5a6c395474cf42c5b3
Oscar Johnson <oscarj415@gmail.com>
2025-03-11 14:51:08 +0100

    add user type

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/delete'
   devise_for :users
   root to: "pages#home"
+
+  resources :events do
+    resources :interests, only:[:create]
+    resources :reviews
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
