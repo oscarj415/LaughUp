9a9ddef290e1e788fa324ca6bfae7e8ae723be9c
Oscar Johnson <oscarj415@gmail.com>
2025-03-11 12:26:46 +0100

    action updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,48 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    set_event
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    set_event
+  end
+
+  def update
+    if @event.update(events_params)
+      redirect_to events_path(@event)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @event.destroy
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def events_params
+    params.require(:event).permit(:xxx, :yyy)
+  end
 end
