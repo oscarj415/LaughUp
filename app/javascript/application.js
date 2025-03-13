8e51c976f5b7e34c545b1b8ca3595ae5f94ce6db
Dariusz Tomaszewski <dd@darekdarko.com>
2025-03-13 16:46:57 +0100

    some styleZZZZ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-
 import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Filter-Container Animation
   const filterBtn = document.querySelector(".filter-btn");
   const closeBtn = document.querySelector(".close-btn");
   const filterContainer = document.querySelector(".filter-container");
@@ -19,43 +18,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Event Listener für das Öffnen des Menüs
   filterBtn.addEventListener("click", () => {
-    // Animate von rechts in den sichtbaren Bereich
     gsap.to(filterContainer, {
       duration: 0.5,
       opacity: 1,
       pointerEvents: "auto",  // Aktiviert Interaktionen
       x: "0%",  // Bewegt den Container in den sichtbaren Bereich
+      ease: "bounce.out",
     });
   });
 
   // Event Listener für das Schließen des Menüs
   closeBtn.addEventListener("click", () => {
-    // Animate zurück nach rechts außerhalb des sichtbaren Bereichs
     gsap.to(filterContainer, {
       duration: 0.5,
       opacity: 0,
       pointerEvents: "none",  // Deaktiviert Interaktionen
       x: "100%",  // Bewegt den Container wieder nach rechts
+      ease: "bounce.out",
     });
   });
 
-
-
-document.addEventListener("DOMContentLoaded", function () {
+  // Navbar Toggler und Close-Button
   const navbarToggler = document.querySelector(".navbar-toggler");
   const navbarCollapse = document.querySelector("#navbarSupportedContent");
   const closeButton = document.querySelector(".close-menu");
 
   if (navbarToggler && navbarCollapse && closeButton) {
+    // Toggler öffnet das Menü
     navbarToggler.addEventListener("click", function () {
       navbarCollapse.classList.add("show");
     });
 
+    // Schließt das Menü über den Close-Button
     closeButton.addEventListener("click", function () {
       navbarCollapse.classList.remove("show");
     });
 
-    // Close navbar when clicking outside of it
+    // Schließt das Menü, wenn man außerhalb des Menüs klickt
     document.addEventListener("click", function (event) {
       let clickedElement = event.target;
       if (navbarCollapse.contains(clickedElement) === false && navbarToggler.contains(clickedElement) === false) {
@@ -63,5 +62,4 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-
 });
