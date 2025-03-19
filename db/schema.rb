f0aef5f2394f45eebc8c1ba3bf9057aea5168c73
Tamari Pilia <tamaripilia13@gmail.com>
2025-03-19 16:03:51 +0100

    edit button style

--- db/schema.rb
+++ db/schema.rb
@@ -98,6 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_19_110633) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url"
     t.float "distance"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
